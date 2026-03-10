261c0d38a2ec3adb6a04a6ef67e2250c987373ab
cdric.B <cdric.b@Mac.lan>
2026-03-10 12:59:58 +0100

    libft_v2

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 23:59:05 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/03/10 00:00:32 by cdric.b          ###   ########.fr       */
+/*   Updated: 2026/03/10 12:50:15 by cdric.b          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list *new_root;
+    t_list *new_node;
     
+    if (!lst|| !f || !del)
+        return (NULL);
+    new_root = ft_lstnew(f(lst->content));
+    if (!new_root)
+        return (NULL);
+    lst = lst->next;
+    while (lst)
+    {
+        new_node = ft_lstnew(f(lst->content));
+        if (!new_node)
+        {
+            ft_lstclear(&new_root,del);
+            return (NULL);
+        }
+        ft_lstadd_back(&new_root, new_node);
+        lst = lst->next;
+    }
+
+    return (new_root);
 }
