926739f54692fcc4544075d8c0bdec98e923355f
cdric.B <cdric.b@cdric.local>
2026-05-01 15:03:43 +0200

    assert_strdup

--- assert/assertion.c
+++ assert/assertion.c
@@ -6,14 +6,17 @@
 /*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 14:20:37 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/05/01 14:38:26 by cdric.b          ###   ########.fr       */
+/*   Updated: 2026/05/01 15:03:05 by cdric.b          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assertion.h"
 
-void clean_str(char *s1, char *s2)
+void test_and_clean_str(char *s1, char *s2)
 {
+
+    if(s1 && s2)
+        assert(!strcmp(s1,s2));
     if(s1)
     {
         s1 = NULL;
@@ -26,7 +29,7 @@ void clean_str(char *s1, char *s2)
     }
 }
 
-void ft_strdup_assert()
+void ft_strdup_assert(void)
 {
     TEST_START;
     char *s1;
@@ -34,15 +37,38 @@ void ft_strdup_assert()
 
     s1 = ft_strdup("");
     s2 = strdup("");
-    assert(!strcmp(s1,s2));
-    clean_str(s1, s2);
+    
     
     s1 = ft_strdup("hello_world");
     s2 = strdup("hello_world");
-    assert(!strcmp(s1,s2));
-    clean_str(s1, s2);
+    test_and_clean_str(s1, s2);
+
+    s1  = strdup("                                                  ");
+    s2  = ft_strdup("                                                  ");
+    test_and_clean_str(s1, s2);
+    
+    char b1[7] = {-10,-10, -10, -10, -10, -10, '\0'};
+    s1 = strdup(b1);    
+    s2 = ft_strdup(b1);
+    assert(s1[0] == -10 && s2[0] == -10);    
+    test_and_clean_str(s1,s2);
+
+    s1 = strdup("\n\t\n\r dqdqwddqw     dwqdwq  dwq $$$$!^");
+    s2 = ft_strdup("\n\t\n\r dqdqwddqw     dwqdwq  dwq $$$$!^");
+    test_and_clean_str(s1,s2);
+    
     TEST_END;
-}   
+}
+
+void ft_atoi_assert(void)
+{
+    TEST_START;
+
+
+    TEST_END;
+}
+
+
 
 
 int main(void)
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 21:39:00 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/03/07 17:37:03 by cdric.b          ###   ########.fr       */
+/*   Updated: 2026/05/01 14:55:25 by cdric.b          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ char	*ft_strdup(const char *s)
 	char	*str;
 	size_t	i;
 
-	if (!s)
-		return (NULL);
 	str = malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
